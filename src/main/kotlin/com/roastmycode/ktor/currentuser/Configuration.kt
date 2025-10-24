e96e2c67482b590925e533077b4ea32d9f82350e
adam-mcguinness <ki4mor@gmail.com>
2025-10-24 17:33:18 -0600

    Updated Config

--- src/main/kotlin/com/roastmycode/ktor/currentuser/Configuration.kt
+++ src/main/kotlin/com/roastmycode/ktor/currentuser/Configuration.kt
@@ -3,13 +3,41 @@ package com.roastmycode.ktor.currentuser
 import kotlinx.serialization.json.Json
 import kotlin.reflect.KClass
 
-class CurrentUserConfiguration {
+enum class AdminSource {
+    ROLE,
+    PERMISSION
+}
+
+class CurrentUserConfiguration{
+    var throwError: Boolean = false
+
+    var errorMessage: String = "You are not authorized to access this."
+
+    var extraction: Extraction = Extraction()
+
+    var adminConfig: AdminConfig? = AdminConfig()
+
+}
+
+class Extraction {
     var json: Json = Json
     internal var metadataClass: KClass<*>? = null
-    
+
+    var user: String = "sub"
+
+    var rolesClaimPath: String = "roles"
+
+    var permissionsClaimPath: String = "permissions"
+
     fun <T : Any> metadata(klass: KClass<T>) {
         metadataClass = klass
     }
+}
+
+class AdminConfig {
+    var adminSource: AdminSource = AdminSource.ROLE
+    var adminRole: String? = "admin"
+    var adminPermission: String? = "admin:super"
 }
 
 inline fun <reified T : Any> CurrentUserConfiguration.metadata() {
